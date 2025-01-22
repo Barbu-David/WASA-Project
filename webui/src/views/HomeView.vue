1104f294c07779669ed51a9b759257d707615bd8
Barbu David <barbu.david.c5a@student.ucv.ro>
2025-01-22 13:09:33 +0100

    stuff

--- webui/src/views/HomeView.vue
+++ webui/src/views/HomeView.vue
@@ -1,38 +1,58 @@
+<template>
+        <div class="sidebar">
+                <input v-model="username" placeholder="Enter your name" />
+                <button v-if="!securityKey" @click="loginUser">Login</button>
+                <button v-else @click="logoutUser">Logout</button>
+                <p v-if="msg">{{ msg }}</p>
+                <div class="black-bar"></div>
+        </div>
+</template>
+
 <script>
 export default {
-	data: function() {
-		return {
-			username: '',
-			msg: '',
-			securityKey: null,
-			userId: null,
-		}
-	},
-	methods: {
-		async loginUser() {
-			try {
-				let response = await this.$axios.post("http://localhost:3000/session", {
-					name: this.username
-				});
-				this.securityKey = response.data.apiKey;
-				this.userId = response.data.userId;
-				this.msg="Logged in as " +this.username;
-			} catch (e) {
-				this.msg = "Login failed: " + e;
-			}
-		}
-	}
+        data: function() {
+                return {
+                        username: '',
+                        msg: '',
+                        securityKey: null,
+                        userId: null,
+                }
+        },
+        methods: {
+                async loginUser() {
+                        try {
+                                let response = await this.$axios.post("http://localhost:3000/session", {
+                                        name: this.username
+                                });
+                                this.securityKey = response.data.apiKey;
+                                this.userId = response.data.userId;
+                                this.msg="Logged in as " +this.username;
+                        } catch (e) {
+                                this.msg = "Login failed: " + e;
+                        }
+                },
+                logoutUser() {
+                        this.username = '';
+                        this.securityKey = null;
+                        this.userId = null;
+                        this.msg = "Logged out successfully";
+                }
+        }
 }
 </script>
 
-<template>
-	<div>
-		<h1 class="h2">Log in</h1>
-		<input v-model="username" placeholder="Enter your name" />
-		<button @click="loginUser">Login</button>
-		<p v-if="msg">{{ msg }}</p>
-	</div>
-</template>
-
 <style>
+.sidebar {
+        width: 200px;
+        background-color: #f4f4f4;
+        padding: 20px;
+}
+
+.black-bar {
+        width: 100%;
+        height: 10px;
+        background-color: black;
+        margin-top: 10px;
+}
 </style>
+
